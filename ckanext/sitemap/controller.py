83e78fcce84bc8c47fac41fcff544c320027dd71
Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>
2012-08-17 17:16:30 +0300

    Initial simple functionality added. Along with tests.

--- ckanext/sitemap/controller.py
+++ ckanext/sitemap/controller.py
@@ -1,8 +1,34 @@
 '''
 Controller for sitemap
 '''
-from ckan.lib.base import BaseController, render
+import logging
+
+from ckan.lib.base import BaseController
+from ckan.model import Session, Package
+from ckan.lib.helpers import url_for
+from lxml import etree
+from pylons import config
+
+SITEMAP_NS = "http://www.sitemaps.org/schemas/sitemap/0.9"
+
+log = logging.getLogger(__file__)
 
 class SitemapController(BaseController):
+
     def view(self):
-        return ''
+        root = etree.Element("urlset", nsmap={None: SITEMAP_NS})
+        pkgs = Session.query(Package).all()
+        for pkg in pkgs:
+            url = etree.SubElement(root, 'url')
+            loc = etree.SubElement(url, 'loc')
+            pkg_url = url_for(controller='package', action="read", id = pkg.name)
+            loc.text = config.get('ckan.site_url') + pkg_url
+            lastmod = etree.SubElement(url, 'lastmod')
+            lastmod.text = pkg.latest_related_revision.timestamp.strftime('%Y-%m-%d')
+            for res in pkg.resources:
+                url = etree.SubElement(root, 'url')
+                loc = etree.SubElement(url, 'loc')
+                loc.text = config.get('ckan.site_url') + url_for(controller="package", action="resource_read", id = pkg.name, resource_id = res.id)
+                lastmod = etree.SubElement(url, 'lastmod')
+                lastmod.text = res.created.strftime('%Y-%m-%d')
+        return etree.tostring(root, pretty_print=True)
--- ckanext/sitemap/tests/test_sitemap.py
+++ ckanext/sitemap/tests/test_sitemap.py
@@ -7,15 +7,23 @@ import unittest
 import mock
 from StringIO import StringIO
 import json
+from lxml import etree
+import datetime
 
-from ckan.model import Session, Package, User
+from pylons import config
+
+from ckan.model import Session, Package, User, Group
 import ckan.model as model
 from ckan.tests import CreateTestData
 from ckan.lib.helpers import url_for
 from ckan.logic.auth.get import package_show
 from ckan.tests.functional.base import FunctionalTestCase
 
+import testdata
+
 log = logging.getLogger(__file__)
+
+siteschema = etree.XMLSchema(etree.parse(StringIO(testdata.sitemap)))
 
 class TestSitemap(FunctionalTestCase, unittest.TestCase):
     
@@ -26,8 +34,10 @@ class TestSitemap(FunctionalTestCase, unittest.TestCase):
         """
         Session.remove()
         CreateTestData.create()
-        cls._first = True
-        cls._second = False
+        url = url_for(controller="ckanext.sitemap.controller:SitemapController",
+                      action='view')
+        cls.cont = cls.app.get(url)
+        cls.content_file = StringIO(cls.cont.body)
 
     @classmethod
     def teardown_class(cls):
@@ -38,7 +48,27 @@ class TestSitemap(FunctionalTestCase, unittest.TestCase):
         Session.remove()
 
     def test_controller(self):
-        url = url_for(controller="ckanext.sitemap.controller:SitemapController", action='view')
-        cont = self.app.get(url)
-        self.assert_(cont.response.status == "200 OK")
+        self.assert_(self.cont.response.status == "200 OK")
+
+    def test_validity(self):
+        siteschema.assertValid(etree.parse(self.content_file))
+        self.assert_(siteschema.validate(etree.parse(self.content_file)))
+
+    def test_packages(self):
+        tree = etree.parse(self.content_file)
+        self.assert_("annakarenina" in self.cont.body)
+        log.debug(self.cont.body)
+        pkg = Session.query(Package).all()[0]
+        urli = config.get('ckan.site_url') + url_for(controller='package',
+                                                     action='read',
+                                                     id = pkg.name)
+        self.assert_(tree.getroot()[0][0].text == urli)
+        # Needs to be created today as test data is too
+        pkgdate = pkg.latest_related_revision.timestamp.strftime('%Y-%m-%d')
+        self.assert_(tree.getroot()[0][1].text == pkgdate)
+        resurl = config.get('ckan.site_url') + url_for(controller='package',
+                                                       action='resource_read',
+                                                       id = pkg.name,
+                                                       resource_id = pkg.resources[0].id)
+        self.assert_(tree.getroot()[1][0].text == resurl)
 
